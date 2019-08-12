d56d7b798fd247aa3d1ea5eaeb48e50b9d6fdf38
ytake38 <chopper0308y@gmail.com>
2019-08-12 13:09:02 +0900

    desu

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ def callback():
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text=event.message.text + "です"))
 
 
 if __name__ == "__main__":
