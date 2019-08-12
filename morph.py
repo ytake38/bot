4b1344d2efe1fa1506f340b4a749e057a29070db
ytake38 <chopper0308y@gmail.com>
2019-08-12 13:32:04 +0900

    morph

--- morph.py
+++ morph.py
@@ -5,5 +5,16 @@ from sudachipy import dictionary
 
 def analize(text):
     tokenizer_obj = dictionary.Dictionary().create()
-    tokens = tokenizer_obj.tokenize(tokenizer.Tokenizer.SplitMpde.C, text)
+    mode = tokenizer.Tokenizer.SplitMode.A
+    tokens = tokenizer_obj.tokenize(text, mode)
+    #tokens = tokenizer_obj.tokenize(tokenizer.Tokenizer.SplitMode.C, text)
     return tokens
+
+if __name__ == "__main__":
+    text = "私は昨日東京に行きました。"
+    tokenizer_obj = dictionary.Dictionary().create()
+    #tokens = tokenizer_obj.tokenize(tokenizer.Tokenizer.SplitMode.C, text)
+    mode = tokenizer.Tokenizer.SplitMode.A
+    tokens = tokenizer_obj.tokenize(text, mode)
+    for t in tokens:
+        print(t.surface(), t.part_of_speech(), t.reading_form(), t.normalized_form())
