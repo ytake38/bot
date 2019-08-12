a5c4260f986f41a373ffe271903022e892c3273f
ytake38 <chopper0308y@gmail.com>
2019-08-12 13:34:09 +0900

    morph

--- main.py
+++ main.py
@@ -43,12 +43,12 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    text = morph.analize(event.message.text)
-    for t in text:
-        line_bot_api.reply_message(
-            event.reply_token,
-            #TextSendMessage(text=event.message.text + "です"))
-            TextSendMessage(t.surface(), t.part_of_speech(), t.reading_form(), t.normalized_form()))
+    t = morph.analize(event.message.text)
+    #    for t in text:
+    line_bot_api.reply_message(
+        event.reply_token,
+        #TextSendMessage(text=event.message.text + "です"))
+        TextSendMessage(t.surface()))
 
 
 if __name__ == "__main__":
